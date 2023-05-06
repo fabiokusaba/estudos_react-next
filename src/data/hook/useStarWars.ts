f4f86cc94376b49e94e51d363de08ff433686061
Fabio Kusaba <fabinhokb44@gmail.com>
2023-05-06 18:26:20 -0300

    Revisão Hooks - Aula 06

--- src/data/hook/useStarWars.ts
+++ src/data/hook/useStarWars.ts
@@ -1,12 +1,30 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import useProcessando from "./useProcessando"
 
 export default function useStarWars() {
     
     const { processando, iniciarProcessamento, finalizarProcessamento } = useProcessando()
-    const [personagens, setPersonagens] = useState<any>([])
+    const [personagens, setPersonagens] = useState<any[]>([])
+    const [personagem, setPersonagem] = useState<any>([])
+    const [filmes, setFilmes] = useState<any>([])
 
-    async function obterPersonagens() {
+    const obterFilmes = useCallback(async function(personagem: any) {
+        if (!personagem.films?.length) return
+        try {
+            iniciarProcessamento()
+            const requests = personagem.films.map(async (film: string) => {
+                const response = await fetch(film)
+                return response.json()
+            })
+    
+            const films = await Promise.all(requests)
+            setFilmes(films)
+        } finally {
+            finalizarProcessamento()
+        }
+    }, [iniciarProcessamento, finalizarProcessamento])
+
+    const obterPersonagens = useCallback(async function () {
         try {
             iniciarProcessamento()
             const resp = await fetch('https://swapi.dev/api/people')
@@ -15,11 +33,30 @@ export default function useStarWars() {
         } finally {
             finalizarProcessamento()
         }
+    }, [iniciarProcessamento, finalizarProcessamento])
+
+    function selecionarPersonagem(personagem: any) {
+        setPersonagem(personagem)
     }
-    
+
+    function voltar() {
+        setPersonagem(null)
+        setFilmes([])
+    }
+
+    useEffect(() => {
+        obterPersonagens()
+    }, [obterPersonagens])
+
+    useEffect(() => {
+        obterFilmes(personagem)
+    }, [personagem, obterFilmes])
+
     return {
         personagens,
-        obterPersonagens,
+        filmes,
         processando,
+        selecionarPersonagem,
+        voltar
     }
 }
--- src/pages/starwars/index.tsx
+++ src/pages/starwars/index.tsx
@@ -1,25 +1,26 @@
 import Background from "@/components/starwars/Background";
+import Filmes from "@/components/starwars/Filmes";
 import Personagens from "@/components/starwars/Personagens";
 import useStarWars from "@/data/hook/useStarWars";
 
 export default function PaginaStarWars() {
 
-    const { processando, personagens, obterPersonagens} = useStarWars()
+    const { processando, personagens, filmes, voltar, selecionarPersonagem } = useStarWars()
 
     return (
-        <div className="flex flex-col gap-5 justify-center items-center h-screen">
+        <div className="flex flex-col gap-5 justify-center items-center h-screen relative">
             <Background />
 
 
             {processando ? (
                 <div>Processando...</div>
+                ) : filmes.length > 0 ? (
+                    <Filmes filmes={filmes} voltar={voltar} />
                 ) : personagens.length > 0 ? (
-                    <Personagens personagens={personagens} />
+                    <Personagens personagens={personagens} selecionar={selecionarPersonagem}/>
                     ) : (
-                        <h1>Nenhum personagem encontrado!</h1>
+                        <h1>Dados não encontrados!</h1>
                         )}
-
-            <button onClick={obterPersonagens} className="bg-blue-500 p-2">Obter</button>
         </div>
     )
 }
